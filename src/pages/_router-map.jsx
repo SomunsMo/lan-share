57dc3012dfb29715a5b1e91177af3dee797dfed9
Somunsm <1373699746@qq.com>
2026-01-21 18:22:53 +0800

    refactor(config): 优化路径别名配置并替换静态资源导入方式

--- src/pages/_router-map.jsx
+++ src/pages/_router-map.jsx
@@ -1,8 +1,13 @@
 // 路由配置
 
-import Home from "./home/index.jsx";
-import Settings from "./settings/index.jsx";
-import TextSharingManager from "./text-sharing-manager/index.jsx";
+import Home from "@/pages/home/index.jsx";
+import Settings from "@/pages/settings/index.jsx";
+import TextSharingManager from "@/pages/text-sharing-manager/index.jsx";
+
+import HomeIcon from "/src/assets/icon/home.svg";
+import TextSharingIcon from "/src/assets/icon/textMsg.svg";
+import SettingIcon from "/src/assets/icon/setting.svg";
+
 
 export const routes = [
     {
@@ -12,19 +17,19 @@ export const routes = [
         path: "/home",
         element: <Home/>,
         name: "主页",
-        icon: "/src/assets/icon/home.svg",
+        icon: HomeIcon,
         navPosition: "top"
     }, {
         path: "/text-sharing",
         element: <TextSharingManager/>,
         name: "文本共享",
-        icon: "/src/assets/icon/textMsg.svg",
+        icon: TextSharingIcon,
         navPosition: "top"
     }, {
         path: "/settings",
         element: <Settings/>,
         name: "设置",
-        icon: "/src/assets/icon/setting.svg",
+        icon: SettingIcon,
         navPosition: "bottom"
     }
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,38 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    plugins: [react()],
+    // 配置路径别名
+    resolve: {
+        alias: {
+            // '@' 指向 项目根目录/src
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent Vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            // 3. tell Vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
+    },
 }));
